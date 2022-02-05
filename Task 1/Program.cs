d7d812fba9319ace0e2cc0aa8f8dd9a30e0cf1ad
unknown <vetrenko.ag@gmail.com>
2022-02-05 13:28:21 +0500

    Tasks

---
+++ Task 1/Program.cs
@@ -0,0 +1,8 @@
+﻿//Вывести квадрат числа
+double result=-1;
+string s;
+
+System.Console.WriteLine("Введите число ");
+s = Console.ReadLine();
+result = Convert.ToDouble(s);
+System.Console.WriteLine($"{result*result,0:F2}");
