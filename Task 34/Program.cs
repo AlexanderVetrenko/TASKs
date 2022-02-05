1a78b6abe4c8564abadd63079b325ee930b05eb6
unknown <vetrenko.ag@gmail.com>
2022-02-05 20:00:46 +0500

    Task 34

---
+++ Task 34/Program.cs
@@ -0,0 +1,18 @@
+﻿// Написать программу замену элементов массива на противоположные
+
+/*вопрос - и что такое "на противоположные"? С противоположным знаком, что ли?
+Ок, буду менять знак */
+
+int [] Arr = new int [12];
+
+for(int i=0; i< Arr.Length; i++)
+{
+    Arr[i] = new Random().Next(-9, 10);
+    System.Console.Write($"{Arr[i]}" + " ");
+    Arr[i] = Arr[i]*(-1);
+}
+System.Console.WriteLine(); // вставляем пустую строчку
+for (int i = 0; i < Arr.Length; i++)
+{
+    System.Console.Write($"{Arr[i]}" + " ");
+}
