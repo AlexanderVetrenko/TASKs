31f340884a8f8801d5bd21f504a73e26592fc2c0
unknown <vetrenko.ag@gmail.com>
2022-02-06 20:56:35 +0500

    Task 6

---
+++ Task 06/Program.cs
@@ -0,0 +1,18 @@
+﻿// Написать программу вычисления значения функции y = f(a)
+// возьмем для примера простую функцию y = 2*x2 + 1
+
+int a = 0;
+string s = String.Empty;
+
+System.Console.WriteLine("Введите аргумент функции ");
+s = Console.ReadLine();
+a = Convert.ToInt32(s);
+
+double Func(int b)
+{
+    double res = 0;
+    res = 2*(Math.Pow(b,2)) + 1;
+    return res;
+}
+
+System.Console.WriteLine("Значение функции " + Func(a));
