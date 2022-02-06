391d347931120c7692d2031128e94e681aaaa168
unknown <vetrenko.ag@gmail.com>
2022-02-06 13:27:46 +0500

    Task 30

---
+++ Task 30/Program.cs
@@ -0,0 +1,23 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+
+int a = 0;
+double n = 0, grade = 0;
+string s;
+
+System.Console.WriteLine("Введите число ");
+s = Console.ReadLine();
+a = Convert.ToInt32(s);
+
+while (a!=0) // будем выполнять программу до тех пор, пока не пользователь не введет 0
+{
+    grade = Math.Pow(a, 3);
+    s = Convert.ToString(grade);
+    n = Convert.ToDouble(s.Length-1);                       
+    if (n%2 == 0)
+    {
+        System.Console.WriteLine($"Куб числа "+ a +" равен " + grade);
+    }
+    System.Console.WriteLine("Введите число ");
+    s = Console.ReadLine();
+    a = Convert.ToInt32(s);
+}
