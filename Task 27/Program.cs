ff9f2eb1f2341373ef36792abcd73befda81599a
unknown <vetrenko.ag@gmail.com>
2022-02-06 13:06:51 +0500

    Task 27

---
+++ Task 27/Program.cs
@@ -0,0 +1,18 @@
+﻿// Определить количество цифр в числе
+
+string s = String.Empty;
+
+System.Console.WriteLine("Введите число ");
+s = Console.ReadLine();
+
+int Quant(string txt)
+{
+    int count  = 0;
+    for (int i = 0; i < txt.Length; i++)
+    {
+        count++;
+    }
+    return count;
+}
+
+System.Console.WriteLine($"Количество цифр в числе равно " + Quant(s));
