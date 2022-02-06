8d30c4ab8a569d25a6dacb8498232113ce56c8b5
unknown <vetrenko.ag@gmail.com>
2022-02-06 21:00:10 +0500

    Task 7

---
+++ Task 07/Program.cs
@@ -0,0 +1,11 @@
+﻿// Выяснить является ли число чётным
+
+int a = 0;
+string s = String.Empty;
+
+System.Console.WriteLine("Введите число ");
+s = Console.ReadLine();
+a = Convert.ToInt32(s);
+
+if(a%2 == 0) System.Console.WriteLine("Введенное число является четным");
+else System.Console.WriteLine("Введенное число является нечетным");
