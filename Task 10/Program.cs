bd35ecae1236ac2ab0ffb84b3806f12c1260dd0e
unknown <vetrenko.ag@gmail.com>
2022-02-07 19:46:07 +0500

    Task 10

---
+++ Task 10/Program.cs
@@ -0,0 +1,11 @@
+﻿// Показать последнюю цифру трёхзначного числа
+
+int a = 0;
+string s = string.Empty;
+
+System.Console.Write("Введите трехзначное число ");
+s = Console.ReadLine();
+a = Convert.ToInt32(s.Length);
+
+if(a == 3) System.Console.WriteLine("Последняя цифра трехзначного числа " + s[s.Length-1]);
+else System.Console.WriteLine("Число не является трехзначным");
