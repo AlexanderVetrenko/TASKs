308d5bb149683e08ea175b71c9ec9b5c80283ddd
unknown <vetrenko.ag@gmail.com>
2022-02-07 18:45:31 +0500

    Task 9

---
+++ Task 09/Program.cs
@@ -0,0 +1,14 @@
+﻿// Показать четные числа от 1 до N
+
+int a = 0;
+string s = String.Empty;
+
+System.Console.WriteLine("Введите число ");
+s = Console.ReadLine();
+a = Convert.ToInt32(s);
+//int [] arr = new int [a+1];
+
+for (int i = 1; i <= a; i++)
+{
+    if(i%2 == 0) System.Console.Write(i + " ");
+}
