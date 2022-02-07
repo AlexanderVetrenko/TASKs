556053c5683b06ffc3640c13cf269a1351cbc42a
unknown <vetrenko.ag@gmail.com>
2022-02-07 19:54:56 +0500

    Task 17

---
+++ Task 17/Program.cs
@@ -0,0 +1,15 @@
+﻿// По двум заданным числам проверять является ли одно квадратом другого
+double a = 0, b = 0;
+string s;
+
+System.Console.WriteLine("Введите первое число ");
+s = Console.ReadLine();
+a = Convert.ToDouble(s);
+
+System.Console.WriteLine("Введите второе число ");
+s = Console.ReadLine();
+b = Convert.ToDouble(s);
+
+if (a == Math.Pow(b,2)) System.Console.WriteLine("Первое число является квадратом второго!");
+if (b == Math.Pow(a,2)) System.Console.WriteLine("Второе число является квадратом первого!");
+if (a != Math.Pow(b,2) && b != Math.Pow(a,2)) System.Console.WriteLine("Ни одно из чисел не является квадратом другого");
