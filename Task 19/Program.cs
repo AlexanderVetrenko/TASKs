da83c1b768c32d6cc847ac37dec806466be8d2aa
unknown <vetrenko.ag@gmail.com>
2022-02-08 20:58:55 +0500

    Task 19

---
+++ Task 19/Program.cs
@@ -0,0 +1,20 @@
+﻿// Определить номер четверти плоскости, в которой находится точка с координатами Х и У, причем X ≠ 0 и Y ≠ 0
+
+int x = 0, y = 0;
+string s = String.Empty;
+
+System.Console.WriteLine("Введите значение Х ");
+s = Console.ReadLine();
+x = Convert.ToInt32(s);
+
+System.Console.WriteLine("Введите значение Y ");
+s = Console.ReadLine();
+y = Convert.ToInt32(s);
+
+if(x > 0 && y > 0) System.Console.WriteLine("Точка с координатами Х = " + x + " Y = " + y + " находится в 1-ой четверти плоскости");
+if(x > 0 && y < 0) System.Console.WriteLine("Точка с координатами Х = " + x + " Y = " + y + " находится в 4-ой четверти плоскости");
+if(x < 0 && y < 0) System.Console.WriteLine("Точка с координатами Х = " + x + " Y = " + y + " находится в 3-ей четверти плоскости");
+if(x < 0 && y > 0) System.Console.WriteLine("Точка с координатами Х = " + x + " Y = " + y + " находится во 2-ой четверти плоскости");
+if(x == 0 && y != 0) System.Console.WriteLine("Точка лежит на оси Х");
+if(x != 0 && y == 0) System.Console.WriteLine("Точка лежит на оси Y");
+if(x == 0 && y == 0) System.Console.WriteLine("Точка находится в начале координат");
