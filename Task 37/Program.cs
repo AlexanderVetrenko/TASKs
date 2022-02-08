dabc5f72c49e3969c7d706202626bd6b3b272ef1
unknown <vetrenko.ag@gmail.com>
2022-02-08 19:47:13 +0500

    Task 37

---
+++ Task 37/Program.cs
@@ -0,0 +1,14 @@
+﻿// В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+
+int [] a = new int [123];
+int count = 0;
+
+for(int i = 0;i < a.Length; i++)
+    a[i]=new Random().Next(0,100);
+
+for (int i = 10; i < 100; i++)
+{
+    count++;
+}
+
+System.Console.WriteLine("Количество элементов из отрезка [10,99] составляет " + count);
