a16b4de4da74d98edc603ebe728fe51249ac72a8
unknown <vetrenko.ag@gmail.com>
2022-02-09 19:09:55 +0500

    Task 29

---
+++ Task 29/Program.cs
@@ -0,0 +1,15 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+int a = 0, res = 1;
+string s = string.Empty;
+
+System.Console.Write("Введите число ");
+s = Console.ReadLine();
+a = Convert.ToInt32(s);
+
+for (int i = 1; i <= a; i++)
+{
+    res = res * i;
+}
+
+System.Console.WriteLine("Произведение чисел от 1 до " + a + " равно "+ res);
