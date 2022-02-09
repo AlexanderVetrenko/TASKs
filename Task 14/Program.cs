5d2b91063f96d5976063a0c732b01b5d17dc7fb3
unknown <vetrenko.ag@gmail.com>
2022-02-09 19:57:29 +0500

    Task 14

---
+++ Task 14/Program.cs
@@ -0,0 +1,18 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток
+
+int a = 0, b = 0;
+string s;
+
+System.Console.WriteLine("Введите число ");
+s = Console.ReadLine();
+a = Convert.ToInt32(s);
+
+System.Console.WriteLine("Введите делитель ");
+s = Console.ReadLine();
+b = Convert.ToInt32(s);
+
+if(a%b == 0)
+{
+    System.Console.WriteLine("Число " + a + " кратно числу " + b);
+}
+else System.Console.WriteLine("Остаток от деления числа " +a+ " на число " +b+" равен " + a%b);
