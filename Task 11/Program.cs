56614d2247c227a6607d6361e1749a8d64d79e1d
unknown <vetrenko.ag@gmail.com>
2022-02-09 20:12:34 +0500

    Поправил баги

--- Task 11/Program.cs
+++ Task 11/Program.cs
@@ -5,7 +5,7 @@ string s = string.Empty;
 
 System.Console.Write("Введите трехзначное число ");
 s = Console.ReadLine();
-a = Convert.ToInt32(s.Length);
+a = s.Length;
 
 if(a == 3) System.Console.WriteLine("Вторая цифра трехзначного числа " + s[s.Length-2]);
 else System.Console.WriteLine("Число не является трехзначным");
