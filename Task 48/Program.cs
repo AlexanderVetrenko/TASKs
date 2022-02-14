f419f9fd27650f5a90c8d77d48a0598aa4cc5904
unknown <vetrenko.ag@gmail.com>
2022-02-14 19:05:20 +0500

    Task 48

---
+++ Task 48/Program.cs
@@ -0,0 +1,46 @@
+﻿// Показать двумерный массив размером m×n заполненный целыми числами
+
+int x = 0, y = 0;
+
+Input(out x, out y);
+int [,] matrix = new int[x,y];
+PrintArray(matrix, x, y);
+FillArray(matrix, x,y);
+System.Console.WriteLine();
+PrintArray(matrix, x,y);
+
+void Input (out int m, out int n)
+{
+    string s = string.Empty;
+
+    System.Console.Write("Введите количество строк массива ");
+    s = Console.ReadLine();
+    m = Convert.ToInt32(s);
+
+    System.Console.Write("Введите количество столбцов массива ");
+    s = Console.ReadLine();
+    n = Convert.ToInt32(s);
+}
+
+void PrintArray (int[,] arr, int a, int b)
+{
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < b; j++)
+        {
+            System.Console.Write($"{arr[i,j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] arr, int a, int b)
+{
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < b; j++)
+        {
+            arr[i,j]=new Random().Next(1,100);
+        }
+    }
+}
