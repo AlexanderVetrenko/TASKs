aa3f280b57595595d33ee62b95344f97c179b22d
unknown <vetrenko.ag@gmail.com>
2022-02-18 20:01:57 +0500

    Начал решать задачу 70 и что-то устал

---
+++ Task 70/Program.cs
@@ -0,0 +1,27 @@
+﻿// Найти сумму цифр числа
+
+int x = 0;
+
+Input(out x);
+PrintResult(x, Solve(x, y));
+
+void Input (out int n)
+{
+    System.Console.WriteLine("Введите число  ");
+    n = Convert.ToInt32(Console.ReadLine());
+    
+}
+
+int Solve(int a)
+{   
+    if (a==b) return b;
+    else return a + Solve(a+1, b);
+}
+
+void PrintResult(int a, int b, int d)
+{
+    System.Console.WriteLine("Сумма элементов от " + a + " до " + b + " равна " + d);
+}
+
+
+
