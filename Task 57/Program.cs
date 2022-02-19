70dc3e3400b30f67eb819d16584192462955547f
unknown <vetrenko.ag@gmail.com>
2022-02-19 13:50:31 +0500

    Начал думать над задачей 57

---
+++ Task 57/Program.cs
@@ -0,0 +1,60 @@
+﻿// Написать программу, упорядочивания по убыванию элементов каждой строки двумерного массива.
+
+
+int x = 0, y = 0;
+
+Input(out x, out y);
+double [,] matrix = new int[x, y];
+FillArray(matrix, x, y);
+PrintArray(matrix);
+System.Console.WriteLine("Массив после упорядочивания строк по убыванию:");
+//ExchangeElemnts(matrix, y);
+PrintArray(matrix);
+
+
+
+void Input(out int m, out int n)
+{
+    string s = string.Empty;
+
+    System.Console.Write("Введите количество строк массива ");
+    s = Console.ReadLine();
+    m = Convert.ToInt32(s);
+
+    System.Console.Write("Введите количество столбцов массива ");
+    s = Console.ReadLine();
+    n = Convert.ToInt32(s);
+}
+void FillArray(double[,] arr, int a, int b)
+{
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < b; j++)
+        {
+            arr[i, j] = Convert.ToDouble(new Random().Next(1, 100));
+        }
+    }
+}
+void PrintArray(double[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            System.Console.Write($"{arr[i, j],4} |");
+        }
+        System.Console.WriteLine();
+    }
+}
+void SortElemnts(double[,] arr)
+{
+    double min = arr[0,0];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (arr[i,j] > min) 
+        }
+    }
+}
+
